44cf6ebeb60c6bf8920e8f7dc8e6c6174cf7c9c5
Programming Hero <programming.hero1@gmail.com>
2023-05-30 07:16:20 -0500

    save images to imgbb server

--- src/pages/Dashboard/AddItem/AddItem.jsx
+++ src/pages/Dashboard/AddItem/AddItem.jsx
@@ -1,13 +1,29 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useForm } from 'react-hook-form';
 
+const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
+
 const AddItem = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`
+
     const onSubmit = data => {
-        console.log(data)
+        
+        const formData = new FormData();
+        formData.append('image', data.image[0])
+
+        fetch(img_hosting_url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => res.json())
+        .then(imgResponse => {
+            console.log(imgResponse)
+        })
+
     };
     console.log(errors);
-
+    console.log(img_hosting_token)
     return (
         <div className="w-full px-10">
             <SectionTitle subHeading="What's new" heading="Add an item" ></SectionTitle>
@@ -25,8 +41,8 @@ const AddItem = () => {
                         <label className="label">
                             <span className="label-text">Category*</span>
                         </label>
-                        <select {...register("category", { required: true })} className="select select-bordered">
-                            <option disabled selected>Pick one</option>
+                        <select defaultValue="Pick One" {...register("category", { required: true })} className="select select-bordered">
+                            <option disabled>Pick One</option>
                             <option>Pizza</option>
                             <option>Soup</option>
                             <option>Salad</option>
