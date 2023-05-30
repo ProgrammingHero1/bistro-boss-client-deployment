7bff1e30ccd9f2deba14587fbbbe7ec446ef27a0
Programming Hero <programming.hero1@gmail.com>
2023-05-29 23:54:19 -0500

    add item form created

--- src/pages/Dashboard/AddItem/AddItem.jsx
+++ src/pages/Dashboard/AddItem/AddItem.jsx
@@ -12,44 +12,46 @@ const AddItem = () => {
         <div className="w-full px-10">
             <SectionTitle subHeading="What's new" heading="Add an item" ></SectionTitle>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-control w-full max-w-xs">
+                <div className="form-control w-full mb-4">
                     <label className="label">
                         <span className="label-text font-semibold">Recipe Name*</span>
                     </label>
-                    <input type="text" placeholder="Recipe Name" 
-                    {...register("name", { required: true, maxLength: 120 })}
-                    className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Recipe Name"
+                        {...register("name", { required: true, maxLength: 120 })}
+                        className="input input-bordered w-full " />
                 </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Category*</span>
-                    </label>
-                    <select {...register("category", { required: true })} className="select select-bordered">
-                        <option disabled selected>Pick one</option>
-                        <option>Pizza</option>
-                        <option>Soup</option>
-                        <option>Salad</option>
-                        <option>Dessert</option>
-                        <option>Drinks</option>
-                    </select>
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text font-semibold">Price*</span>
-                    </label>
-                    <input type="number" {...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="flex my-4">
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text">Category*</span>
+                        </label>
+                        <select {...register("category", { required: true })} className="select select-bordered">
+                            <option disabled selected>Pick one</option>
+                            <option>Pizza</option>
+                            <option>Soup</option>
+                            <option>Salad</option>
+                            <option>Dessert</option>
+                            <option>Drinks</option>
+                        </select>
+                    </div>
+                    <div className="form-control w-full ml-4">
+                        <label className="label">
+                            <span className="label-text font-semibold">Price*</span>
+                        </label>
+                        <input type="number" {...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
+                    </div>
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Recipe Details</span>
                     </label>
-                    <textarea {...register("details", { required: true })} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+                    <textarea {...register("recipe", { required: true })} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
                 </div>
-                <div className="form-control w-full max-w-xs">
+                <div className="form-control w-full my-4">
                     <label className="label">
                         <span className="label-text">Item Image*</span>
                     </label>
-                    <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
+                    <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full " />
                 </div>
                 <input className="btn btn-sm mt-4" type="submit" value="Add Item" />
             </form>
