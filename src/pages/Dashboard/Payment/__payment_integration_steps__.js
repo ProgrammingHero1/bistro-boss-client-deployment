3bd553efe59245dd8828605b380ad72cc7f54766
Programming Hero <programming.hero1@gmail.com>
2023-05-30 22:18:56 -0500

    updated steps for stripe

--- src/pages/Dashboard/Payment/__payment_integration_steps__.js
+++ src/pages/Dashboard/Payment/__payment_integration_steps__.js
@@ -2,5 +2,12 @@
  * 1. install stripe and react stripe js
  * 2. create a checkout form with card element (card element contains: card number, expiration date, cvs, zip code)
  * 3. create account on Stripe and get the publishable key pk
- * 4. 
+ * 4. get card information
+ * 5. create a payment method
+ * 6. use test card to test payment
+ * 7. on the server side: install stripe:
+ * npm install --save stripe
+ * 8. create a payment intent api with payment method types: ['card]
+ * make sure you provide amount in cents (multiply price with 100)
+ * 
 */
