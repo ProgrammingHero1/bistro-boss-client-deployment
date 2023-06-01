d817f9936d944bc9a51653eab0add0ef1fc09bb0
Programming Hero <programming.hero1@gmail.com>
2023-05-31 22:55:09 -0500

    load admin stats data

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -24,14 +24,14 @@ const Dashboard = () => {
 
                     {
                         isAdmin ? <>
-                            <li><NavLink to="/dashboard/home"><FaHome></FaHome> Admin Home</NavLink></li>
+                            <li><NavLink to="/dashboard/adminhome"><FaHome></FaHome> Admin Home</NavLink></li>
                             <li><NavLink to="/dashboard/addItem"> <FaUtensils></FaUtensils> Add an Item</NavLink></li>
                             <li><NavLink to="/dashboard/manageitems"><FaWallet></FaWallet> Manage Items</NavLink></li>
                             <li><NavLink to="/dashboard/history"><FaBook></FaBook> Manage Bookings</NavLink></li>
                             <li><NavLink to="/dashboard/allusers"><FaUsers></FaUsers> All Users</NavLink></li>
                             
                         </> : <>
-                            <li><NavLink to="/dashboard/home"><FaHome></FaHome> User Home</NavLink></li>
+                            <li><NavLink to="/dashboard/userhome"><FaHome></FaHome> User Home</NavLink></li>
                             <li><NavLink to="/dashboard/reservations"><FaCalendarAlt></FaCalendarAlt> Reservations</NavLink></li>
                             <li><NavLink to="/dashboard/history"><FaWallet></FaWallet> Payment History</NavLink></li>
                             <li>
--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import useAuth from "../../../hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const AdminHome = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+
+    const { data: stats ={} } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure('/admin-stats');
+            return res.data;
+        }
+    })
+
     return (
         <div className="w-full m-4">
-            <h2 className="text-3xl">Welcome back, {user.displayName}</h2>
+            <h2 className="text-3xl">Hi, {user.displayName}</h2>
+            
         </div>
     );
 };
